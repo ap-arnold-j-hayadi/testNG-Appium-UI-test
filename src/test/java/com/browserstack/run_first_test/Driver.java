cccd05015ba82dff0fca14c255ab4261c2bdc9e3
Arnold Jason Hayadi <arnold.j.hayadi@astrapay.com>
2022-12-02 10:46:16 +0700

    create driver class

---
+++ src/test/java/com/browserstack/run_first_test/Driver.java
@@ -0,0 +1,79 @@
+package com.browserstack.run_first_test;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Driver {
+    private static Driver driverInstance = null;
+    private AndroidDriver driver;
+
+    private Driver() throws IOException, ParseException {
+        System.out.println("Setting Up Test. . . .");
+
+        JSONParser parser = new JSONParser();
+        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/com/browserstack/run_first_test/first.conf.json"));
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        JSONArray envs = (JSONArray) config.get("environments");
+        Map<String, String> envCapabilities = (Map<String, String>) envs.get(0);
+        Iterator it = envCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        }
+
+        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+        it = commonCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if(capabilities.getCapability(pair.getKey().toString()) == null){
+                capabilities.setCapability(pair.getKey().toString(), pair.getValue());
+            }
+        }
+
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        if(username == null) {
+            username = (String) config.get("username");
+        }
+
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if(accessKey == null) {
+            accessKey = (String) config.get("access_key");
+        }
+
+        String app = System.getenv("BROWSERSTACK_APP_ID");
+        if(app != null && !app.isEmpty()) {
+            capabilities.setCapability("app", app);
+        }
+
+        driver = new io.appium.java_client.android.AndroidDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
+//        driver.terminateApp("com.ada.astrapayupdate");
+    }
+
+    public static Driver getInstance() throws IOException, ParseException {
+        if (driverInstance == null) {
+            System.out.println("New Driver Starting. . . .");
+            driverInstance = new Driver();
+        } else {
+            System.out.println("Driver Instance Already Exist. . . .");
+        }
+
+        return driverInstance;
+    }
+
+    public AndroidDriver getDriver() {
+        return driver;
+    }
+
+}
